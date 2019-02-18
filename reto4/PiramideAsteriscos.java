245814e63293f0acbe5e225cea5dc9d31e32a759
bisai96 <bisai.rosa@gmail.com>
2019-02-17 20:17:48 -0600

    Reto4 final

---
+++ reto4/PiramideAsteriscos.java
@@ -0,0 +1,27 @@
+
+package reto4;
+import java.util.Scanner;
+
+public class PiramideAsteriscos {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduzca numero de filas: ");
+        int numFilas = sc.nextInt();
+        sc.close();
+        
+        System.out.println();
+        
+        for(int numBlancos = numFilas-1,numAsteriscos=1; numBlancos>=0; numBlancos--, numAsteriscos += 2){
+            //Espacios en blanco
+            for(int i=1;i<=numBlancos;i++){
+                System.out.println(" ");
+            }
+            //Asteriscos
+            for(int j=1;j<=numAsteriscos;j++){
+                System.out.println("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
